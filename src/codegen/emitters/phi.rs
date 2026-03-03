2e432cf0ef519e7e3999315f3fbc1e3d2a0b52d2
Gabriel Pureliani <gafureliani@gmail.com>
2026-03-03 10:01:20 +0400

    refactor: phi codegen

--- src/codegen/emitters/phi.rs
+++ src/codegen/emitters/phi.rs
@@ -4,11 +4,7 @@ use inkwell::values::PhiValue;
 
 use crate::{
     codegen::CodeGenerator,
-    globals::STRING_INTERNER,
-    hir::{
-        builders::{BasicBlock, BasicBlockId, ValueId},
-        instructions::Terminator,
-    },
+    hir::builders::{BasicBlock, ValueId},
 };
 
 impl<'ctx> CodeGenerator<'ctx> {
@@ -38,7 +34,7 @@ impl<'ctx> CodeGenerator<'ctx> {
     }
 
     pub fn resolve_phis_for_block(
-        &mut self,
+        &self,
         block: &BasicBlock,
         phi_nodes: &HashMap<ValueId, PhiValue<'ctx>>,
     ) {
@@ -52,7 +48,7 @@ impl<'ctx> CodeGenerator<'ctx> {
 
                 for source in sources {
                     let incoming_val = self.get_val_strict(source.value);
-                    let mut incoming_bb = *self
+                    let incoming_bb = self
                         .fn_blocks
                         .get(&source.from)
                         .expect("Phi source block not found");
@@ -63,91 +59,21 @@ impl<'ctx> CodeGenerator<'ctx> {
                         .get(&source.value)
                         .expect("Source type missing");
 
-                    let final_val = if src_ty != phi_ty {
-                        let fn_name = STRING_INTERNER
-                            .resolve(self.current_fn.unwrap().identifier.name);
-                        let current_fn = self.module.get_function(&fn_name).unwrap();
+                    if src_ty != phi_ty {
+                        panic!(
+                            "INTERNAL COMPILER ERROR: Phi node type mismatch in Codegen.\n\
+                             Phi ID: {:?}\n\
+                             Phi Type: {:?}\n\
+                             Source Value: {:?}\n\
+                             Source Type: {:?}\n\
+                             Source Block: {:?}\n\
+                             HIR should have inserted explicit casts or split edges.",
+                            phi_val_id, phi_ty, source.value, src_ty, source.from
+                        );
+                    }
 
-                        let split_bb =
-                            self.context.append_basic_block(current_fn, "edge_split");
-
-                        self.builder.position_at_end(split_bb);
-                        let casted_val = self.emit_cast(incoming_val, src_ty, phi_ty);
-
-                        let phi_bb = *self.fn_blocks.get(&block.id).unwrap();
-                        self.builder.build_unconditional_branch(phi_bb).unwrap();
-
-                        let pred_hir_block =
-                            self.current_fn.unwrap().blocks.get(&source.from).unwrap();
-
-                        if let Some(term) = &pred_hir_block.terminator {
-                            self.redirect_terminator(
-                                incoming_bb,
-                                term,
-                                block.id,
-                                split_bb,
-                            );
-                        }
-
-                        incoming_bb = split_bb;
-                        casted_val
-                    } else {
-                        incoming_val
-                    };
-
-                    phi.add_incoming(&[(&final_val, incoming_bb)]);
+                    phi.add_incoming(&[(&incoming_val, *incoming_bb)]);
                 }
-            }
-        }
-    }
-
-    fn redirect_terminator(
-        &self,
-        llvm_bb: inkwell::basic_block::BasicBlock<'ctx>,
-        hir_term: &Terminator,
-        old_target_id: BasicBlockId,
-        new_target_bb: inkwell::basic_block::BasicBlock<'ctx>,
-    ) {
-        if let Some(term_instr) = llvm_bb.get_terminator() {
-            term_instr.erase_from_basic_block();
-        }
-
-        self.builder.position_at_end(llvm_bb);
-
-        match hir_term {
-            Terminator::Jump { target } => {
-                let dest = if *target == old_target_id {
-                    new_target_bb
-                } else {
-                    *self.fn_blocks.get(target).unwrap()
-                };
-                self.builder.build_unconditional_branch(dest).unwrap();
-            }
-            Terminator::CondJump {
-                condition,
-                true_target,
-                false_target,
-            } => {
-                let cond = self.get_val_strict(*condition).into_int_value();
-
-                let t_dest = if *true_target == old_target_id {
-                    new_target_bb
-                } else {
-                    *self.fn_blocks.get(true_target).unwrap()
-                };
-
-                let f_dest = if *false_target == old_target_id {
-                    new_target_bb
-                } else {
-                    *self.fn_blocks.get(false_target).unwrap()
-                };
-
-                self.builder
-                    .build_conditional_branch(cond, t_dest, f_dest)
-                    .unwrap();
-            }
-            Terminator::Return { .. } => {
-                panic!("INTERNAL COMPILER ERROR: Phi source block cannot end with Return")
             }
         }
     }
